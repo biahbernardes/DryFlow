d879a93d1ed28c3e599d6afeed092deaa2913ddf
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-05-13 14:56:10 -0300

    cadastro com BD vinculado

--- Geral/site/DryFlow/web-data-viz/src/models/empresaModel.js
+++ Geral/site/DryFlow/web-data-viz/src/models/empresaModel.js
@@ -1,13 +1,13 @@
 var database = require("../database/config");
 
 function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+  var instrucaoSql = `SELECT * FROM oficina WHERE idOficina = '${id}'`;
 
   return database.executar(instrucaoSql);
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+  var instrucaoSql = `SELECT idOficina, nomeOficina, codigo_ativacao FROM oficina`;
 
   return database.executar(instrucaoSql);
 }
@@ -18,8 +18,8 @@ function buscarPorCnpj(cnpj) {
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(razaoSocial, cnpj) {
-  var instrucaoSql = `INSERT INTO empresa (razao_social, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
+function cadastrar(nomeFantasia, cnpj) {
+  var instrucaoSql = `INSERT INTO empresa (nomeFantasia, cnpj) VALUES ('${nomeFantasia}', '${cnpj}')`;
 
   return database.executar(instrucaoSql);
 }
