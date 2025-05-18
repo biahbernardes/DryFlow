d9ae107307a84a81a4099c69a52e7cd23ed45522
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-05-18 09:24:20 -0300

    ajustes medidaModel.js

--- Geral/site/DryFlow/web-data-viz/src/models/medidaModel.js
+++ Geral/site/DryFlow/web-data-viz/src/models/medidaModel.js
@@ -10,11 +10,12 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     WHERE fk_aquario = ${idAquario}
                     ORDER BY id DESC LIMIT ${limite_linhas}`; */
   var instrucaoSql = `SELECT  
-        dht11_umidade as umidade,
-                        DATE_FORMAT('2025-05-16','%H:%i:%s') as momento_grafico
+        umidadeRegistrada as umidade,
+        dtHrRegistrada,
+                        DATE_FORMAT(dtHrRegistrada,'%H:%i:%s') as momento_grafico
                     FROM registroSensor
                     WHERE fkSensor = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+                    ORDER BY idRegistro DESC LIMIT ${limite_linhas}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
@@ -29,11 +30,11 @@ function buscarMedidasEmTempoReal(idAquario) {
                         FROM medida WHERE fk_aquario = ${idAquario} 
                     ORDER BY id DESC LIMIT 1`; */
   var instrucaoSql = `SELECT 
-        dht11_umidade as umidade,
-                        DATE_FORMAT('2025-05-16','%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+        umidadeRegistrada as umidade,
+                        DATE_FORMAT(dtHrRegistrada,'%H:%i:%s') as momento_grafico, 
+                        fkSensor 
+                        FROM registroSensor WHERE fkSensor = ${idAquario} 
+                    ORDER BY idRegistro DESC LIMIT 1`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
